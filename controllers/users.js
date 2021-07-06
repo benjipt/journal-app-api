7383969e00ba95a8ef99d4b9450fede87d97402a
Benji Thompson <thompson.benji@gmail.com>
2021-07-06 17:25:53 -0400

    delete route works on users controller

--- controllers/users.js
+++ controllers/users.js
@@ -50,5 +50,20 @@ users.put('/:id', (req, res) => {
     })
 });
 
+// DELETE ROUTE
+/*
+curl -X DELETE \
+    'http://localhost:3000/journals/60e4c61d2b0fa27594204815'
+*/
+users.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id, (err, deletedUser) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        }
+        res.status(200).json({
+            'deleted_user': deletedUser
+        });
+    })
+});
 
 module.exports = users;

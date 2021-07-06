895ed26e5afea6524d8b4e7d1882616b17ee8c28
Benji Thompson <thompson.benji@gmail.com>
2021-07-06 17:08:09 -0400

    Create route works on users controller

--- controllers/users.js
+++ controllers/users.js
@@ -9,4 +9,20 @@ users.get('/', (req, res) => {
     res.send('connected to users');
 });
 
+// CREATE ROUTE
+/*
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"userName":"testUser", "userPassword":"n4tpyh2c47y6vjjabcpz8ptgza"}' \
+    'http://localhost:3000/users'
+*/
+users.post('/', (req, res) => {
+    User.create(req.body, (err, createdUser) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        }
+        res.status(200).json(createdUser);
+    })
+});
+
 module.exports = users;

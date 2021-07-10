463ce83efddb9037c585b0f22184b2b0dc0e689d
Benji Thompson <thompson.benji@gmail.com>
2021-07-09 20:52:39 -0400

    update to .env

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 // App configuration
 const app = express();
 const PORT = process.env.PORT || 3003;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/journals_auth';
 
 // Express Middleware
 app.use(express.urlencoded({ extended: true }));
